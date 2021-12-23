146c95d50afd47c5aeca967b367d9db77f30b1e7
Dan Knights <daniel.knights@28b.co.uk>
2021-12-23 07:53:41 +0000

    fix: update oak dependency

--- src/config/deps.ts
+++ src/config/deps.ts
@@ -1,12 +1,12 @@
 import { config } from 'https://deno.land/x/dotenv@v3.1.0/mod.ts';
 
-export { Application, Router } from 'https://deno.land/x/oak@v10.0.0/mod.ts';
+export { Application, Router } from 'https://deno.land/x/oak@v10.1.0/mod.ts';
 export type {
   Request,
   Context,
   RouterContext,
   RouterMiddleware,
-} from 'https://deno.land/x/oak@v10.0.0/mod.ts';
+} from 'https://deno.land/x/oak@v10.1.0/mod.ts';
 export type { Collection, Filter } from 'https://deno.land/x/mongo@v0.29.0/mod.ts';
 export { Bson, MongoClient } from 'https://deno.land/x/mongo@v0.29.0/mod.ts';
 export { create, verify, decode } from 'https://deno.land/x/djwt@v2.4/mod.ts';
